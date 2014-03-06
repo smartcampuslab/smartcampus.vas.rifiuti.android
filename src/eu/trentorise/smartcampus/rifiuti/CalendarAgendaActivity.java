b1da75da4442450b6df1ff2e02a1e009dd97986c
o.zambotti <o.zambotti@trentorise.eu>
2014-03-06 17:33:45 +0100

    A couple of issues fixed for CalendarAgenda

--- src/eu/trentorise/smartcampus/rifiuti/CalendarAgendaActivity.java
+++ src/eu/trentorise/smartcampus/rifiuti/CalendarAgendaActivity.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 
 import com.tyczj.extendedcalendarview.Day;
 
+import eu.trentorise.smartcampus.rifiuti.model.CalendarioEvent;
 import eu.trentorise.smartcampus.rifiuti.utils.ArgUtils;
 
 public class CalendarAgendaActivity extends ActionBarActivity {
@@ -23,30 +22,15 @@ public class CalendarAgendaActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_calendaragenda);
 		mContentFrameId = R.id.content_frame;
 
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setHomeButtonEnabled(true);
-
 		Intent intent = getIntent();
 		if (intent.hasExtra(ArgUtils.ARGUMENT_CALENDAR_DAY)) {
-			Day day = (Day) intent.getSerializableExtra(ArgUtils.ARGUMENT_CALENDAR_DAY);
+			Day<CalendarioEvent> day = (Day<CalendarioEvent>) intent.getSerializableExtra(ArgUtils.ARGUMENT_CALENDAR_DAY);
 			Fragment fragment = new CalendarAgendaFragment();
 			Bundle bundle = new Bundle();
 			bundle.putSerializable(ArgUtils.ARGUMENT_CALENDAR_DAY, day);
 			fragment.setArguments(bundle);
 			getSupportFragmentManager().beginTransaction().replace(mContentFrameId, fragment).commit();
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return super.onOptionsItemSelected(item);
 	}
 
 }
--- src/eu/trentorise/smartcampus/rifiuti/CalendarAgendaFragment.java
+++ src/eu/trentorise/smartcampus/rifiuti/CalendarAgendaFragment.java
@@ -1,26 +1,44 @@
 package eu.trentorise.smartcampus.rifiuti;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tyczj.extendedcalendarview.Day;
 
+import eu.trentorise.smartcampus.rifiuti.model.CalendarioEvent;
 import eu.trentorise.smartcampus.rifiuti.utils.ArgUtils;
 
 public class CalendarAgendaFragment extends Fragment {
 
-	private Day Day;
+	private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE dd/MM/yyyy", Locale.getDefault());
 
+	private ActionBarActivity abActivity;
+	private Day<CalendarioEvent> day;
+	private Calendar cal;
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		abActivity = (ActionBarActivity) getActivity();
+
+		setHasOptionsMenu(true);
+		abActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		abActivity.getSupportActionBar().setHomeButtonEnabled(true);
 
 		if (getArguments().containsKey(ArgUtils.ARGUMENT_CALENDAR_DAY)) {
-			Day = (Day) getArguments().getSerializable(ArgUtils.ARGUMENT_CALENDAR_DAY);
+			day = (Day<CalendarioEvent>) getArguments().getSerializable(ArgUtils.ARGUMENT_CALENDAR_DAY);
+			cal = Calendar.getInstance(Locale.getDefault());
+			cal.set(day.getYear(), day.getMonth(), day.getDay());
 		}
 	}
 
@@ -34,7 +52,16 @@ public class CalendarAgendaFragment extends Fragment {
 	public void onStart() {
 		super.onStart();
 
-		((ActionBarActivity) getActivity()).getSupportActionBar().setTitle("" + Day.getDay());
+		abActivity.getSupportActionBar().setTitle(dateFormatter.format(cal.getTime()));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			abActivity.finish();
+			return true;
+		}
+		return false;
 	}
 
 }
