7879df0a468d3ce48fbc8279d93e61e534589547
raman <raman@fbk.eu>
2014-03-08 22:01:14 +0100

    cleanup

--- src/eu/trentorise/smartcampus/rifiuti/PuntiDiRaccoltaListFragment.java
+++ src/eu/trentorise/smartcampus/rifiuti/PuntiDiRaccoltaListFragment.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
