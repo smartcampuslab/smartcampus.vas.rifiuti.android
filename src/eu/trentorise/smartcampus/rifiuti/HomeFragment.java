bc1386333cfaaaed45f2597c1fcaac5584d65b3e
chin8 <m.chini@trentorise.eu>
2014-03-11 10:31:47 +0100

    merge

--- src/eu/trentorise/smartcampus/rifiuti/HomeFragment.java
+++ src/eu/trentorise/smartcampus/rifiuti/HomeFragment.java
@@ -1,16 +1,28 @@
 package eu.trentorise.smartcampus.rifiuti;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.github.espiandev.showcaseview.ListViewTutorialHelper;
+import com.github.espiandev.showcaseview.TutorialHelper;
+import com.github.espiandev.showcaseview.TutorialHelper.TutorialProvider;
+import com.github.espiandev.showcaseview.TutorialItem;
+
 import eu.trentorise.smartcampus.rifiuti.data.NotesHelper;
+import eu.trentorise.smartcampus.rifiuti.data.RifiutiHelper;
 
 public class HomeFragment extends Fragment {
 
@@ -22,6 +34,11 @@ public class HomeFragment extends Fragment {
 	private PagerTabStrip mPagerStrip;
 	private ActionBarActivity abActivity;
 	private Integer pagerPreviousItem = null;
+	private TutorialHelper mTutorialHelper = null;
+	public static DrawerLayout mDrawerLayout;
+	private boolean tutorialHasOpened = false;
+	public static ListView mDrawerList;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +48,15 @@ public class HomeFragment extends Fragment {
 		setHasOptionsMenu(true);
 		abActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		abActivity.getSupportActionBar().setHomeButtonEnabled(true);
+		
+		mTutorialHelper = new ListViewTutorialHelper(RifiutiTutorialActivity.this, mTutorialProvider);
+
+		if (RifiutiHelper.isFirstLaunch(getActivity())) {
+			openNavDrawerIfNeeded();
+			
+			showTourDialog();
+			RifiutiHelper.disableFirstLaunch(getActivity());
+		}
 	}
 
 	@Override
@@ -131,5 +157,73 @@ public class HomeFragment extends Fragment {
 		}
 
 	}
+private TutorialProvider mTutorialProvider = new TutorialProvider() {
+		
+		TutorialItem[] tutorial = new TutorialItem[]{
+				new TutorialItem("home", null, 0, R.string.home_title, R.string.home_tut),
+				new TutorialItem("punti raccolta", null, 0, R.string.punti_raccolta_title, R.string.punti_raccolta_tut),
+				new TutorialItem("tipi raccolta", null, 0, R.string.tipi_raccolta_title, R.string.tipi_raccolta_tut),
+				new TutorialItem("gestione profili", null, 0, R.string.gestione_profili_title, R.string.gestione_profili_tut),
+				new TutorialItem("segnala", null, 0, R.string.segnala_title, R.string.segnala_tut),
+				new TutorialItem("contatti", null, 0, R.string.contatti_title, R.string.contatti_tut),
+				new TutorialItem("tutorial", null, 0, R.string.tutorial_title, R.string.tutorial_tut),
+				new TutorialItem("info", null, 0, R.string.info_title, R.string.info_tut),
 
+
+
+}; 
+		@Override
+		public void onTutorialFinished() {
+			mDrawerLayout.closeDrawer(mDrawerList);
+		}
+		
+		@Override
+		public void onTutorialCancelled() {
+			mDrawerLayout.closeDrawer(mDrawerList);
+		}
+		
+		@Override
+		public TutorialItem getItemAt(int i) {
+			ListViewTutorialHelper.fillTutorialItemParams(tutorial[i], i, mDrawerList, R.id.drawer_menu_item);
+			return tutorial[i];
+		}
+		
+		@Override
+		public int size() {
+			return tutorial.length;
+		}
+	};
+	
+	private boolean openNavDrawerIfNeeded() {
+		DrawerLayout mDrawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+		if (!mDrawerLayout.isDrawerOpen(Gravity.LEFT))
+		{
+			tutorialHasOpened=true;
+			mDrawerLayout.openDrawer(Gravity.LEFT);
+			return true;
+			
+		}
+		return false;
+	}
+	
+	private void showTourDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setMessage("bla bla")
+				.setPositiveButton(getString(R.string.begin_tut), new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						RifiutiHelper.setWantTour(getActivity(), true);
+						mTutorialHelper.showTutorials();
+//						showTutorial();
+					}
+				}).setNeutralButton(getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						RifiutiHelper.setWantTour(getActivity(), false);
+						dialog.dismiss();
+					}
+				});
+		builder.create().show();
+	}
 }
