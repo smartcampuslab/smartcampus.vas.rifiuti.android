f88df9c81bb0b97ec3c557a2c6dda1d9ebd56a87
chin8 <m.chini@trentorise.eu>
2014-03-14 12:06:14 +0100

    Info fragment final version

--- src/eu/trentorise/smartcampus/rifiuti/InfoFragment.java
+++ src/eu/trentorise/smartcampus/rifiuti/InfoFragment.java
@@ -1,10 +1,13 @@
 package eu.trentorise.smartcampus.rifiuti;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class InfoFragment extends Fragment{
 
@@ -32,7 +35,56 @@ public class InfoFragment extends Fragment{
 	@Override
 	public void onStart() {
 		super.onStart();
-
+		ImageView smartcampus = (ImageView)getActivity().findViewById(R.id.smartcampus);
+		smartcampus.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.smartcampuslab.it/"));
+		        startActivity(intent);
+		    }
+		});
+		ImageView cct = (ImageView)getActivity().findViewById(R.id.cct);
+		cct.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.comunitrentini.it/"));
+		        startActivity(intent);
+		    }
+		});
+		ImageView giudicarie = (ImageView)getActivity().findViewById(R.id.giudicarie);
+		giudicarie.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.comunitadellegiudicarie.it/"));
+		        startActivity(intent);
+		    }
+		});
+		ImageView ladurner = (ImageView)getActivity().findViewById(R.id.ladurner);
+		ladurner.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.ladurnerambiente.it"));
+		        startActivity(intent);
+		    }
+		});
+		ImageView fbk = (ImageView)getActivity().findViewById(R.id.fbk);
+		fbk.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("https://www.fbk.eu/"));
+		        startActivity(intent);
+		    }
+		});
 	}
 
 	@Override
